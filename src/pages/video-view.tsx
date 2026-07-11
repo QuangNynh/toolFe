3f55278907b78be3581cc462721abfd0a14196fd
QuangNynh <vunynh28@gmail.com>
2026-07-11 21:23:49 +0700

    udd

--- src/pages/video-view.tsx
+++ src/pages/video-view.tsx
@@ -69,9 +69,7 @@ const VideoViewPages = () => {
       const day = date.getDate().toString().padStart(2, '0')
       const month = (date.getMonth() + 1).toString().padStart(2, '0')
       const year = date.getFullYear()
-      const hours = date.getHours().toString().padStart(2, '0')
-      const minutes = date.getMinutes().toString().padStart(2, '0')
-      return `${day}/${month}/${year} ${hours}:${minutes}`
+      return `${day}/${month}/${year}`
     } catch {
       return dateString
     }
